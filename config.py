2d77740cc6ccd5cf83e533b7401d8b46eaac762e
Stakelimbo <stakelimbo46@gmail.com>
2024-07-31 15:48:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -64,7 +64,7 @@ SHORTLINK_URL = environ.get("SHORTLINK_URL", "hRPS5vvZc0OGOEUQJMJzPiojoVK2") # s
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/How_To_Open_Linkl") # how to open link 
 
 # Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
+WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 WEBSITE_URL = environ.get("WEBSITE_URL", "https://moviesunofficialxyz.blogspot.com/2024/07/movies-unofficial.html") # For More Information Check Video On Yt - @Tech_VJ

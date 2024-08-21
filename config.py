f934ac65c28a773ada714ba1607f32e12dd4d8a7
Stakelimbo <stakelimbo46@gmail.com>
2024-08-21 10:54:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,11 +56,11 @@ BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
 
 # Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_API = environ.get("SHORTLINK_API", "api.shareus.io") # shortlink api
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "hRPS5vvZc0OGOEUQJMJzPiojoVK2") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "publicearn.com") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "931f4b13bd82a38c75aa92face2613e5dad9bd4d") # shortlink domain without https://
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/How_To_Open_Linkl") # how to open link 
 
 # Website Info:

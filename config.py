cca59439defb24c2135c41287d39f8a9c8a0c02f
Stakelimbo <stakelimbo46@gmail.com>
2024-08-22 15:44:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,8 +59,8 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_API = environ.get("SHORTLINK_API", "931f4b13bd82a38c75aa92face2613e5dad9bd4d") # shortlink api
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "publicearn.com") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "0f7777033dd56b9b7386ec80165eb072d855bcae") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "modijiurl.com") # shortlink domain without https://
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Tutolinkboo/105") # how to open link 
 
 # Website Info:

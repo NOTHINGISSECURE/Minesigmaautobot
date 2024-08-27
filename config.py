41006b8b18e3711e631a1b71d9a339347713a7b8
NOTHINGISSECURE <154494591+NOTHINGISSECURE@users.noreply.github.com>
2024-08-27 10:07:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -87,5 +87,5 @@ URL = environ.get("URL", "")
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-    
+
+   

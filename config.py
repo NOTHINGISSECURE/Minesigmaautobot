4f98885ed85dee5e986894570d0de9a77bb5eef2
NOTHINGISSECURE <154494591+NOTHINGISSECURE@users.noreply.github.com>
2024-09-09 14:47:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -57,7 +57,7 @@ BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
 
 # Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
+VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 SHORTLINK_API = environ.get("SHORTLINK_API", "f57e6c2cccef526fc0bc1070c238e7be00d92356") # shortlink api

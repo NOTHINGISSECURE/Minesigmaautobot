5eddfa8a7e8a9c1e7194252b0f4a50b54e6345da
NOTHINGISSECURE <154494591+NOTHINGISSECURE@users.noreply.github.com>
2024-09-20 21:56:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 SHORTLINK_API = environ.get("SHORTLINK_API", "f57e6c2cccef526fc0bc1070c238e7be00d92356") # shortlink api
 SHORTLINK_URL = environ.get("SHORTLINK_URL", "kingurl.in") # shortlink domain without https://
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/modijiurltutorial/6") # how to open link 
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", " ") # how to open link 
 
 # Website Info:
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
